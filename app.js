4b2427d7dda7bd518608b42e8e85d83e9d767088
Shreya-Singh <shreyasingh14062000@gmail.com>
2023-10-02 13:21:43 +0530

    second commit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function onSubmit(e){
     }
     else{
         const itemId = new Date().getTime();
+        //li.setAttribute('data-myObj_ser-id', itemId.toString());
       let myObj ={
       name : nam.value,
     disp : dis.value,
@@ -23,12 +24,13 @@ function onSubmit(e){
     axios.post("https://crudcrud.com/api/e955e70c96b34dd99e0cb5bfb20135d2/appoinmentData", myObj)
   .then((response) => {
     // Extract the relevant fields from response.data
+    console.log(response);
     const responseData = response.data;
     const { name, disp, category } = responseData;
-
+    //console.log(responseData);
     // Create a text node to display the data
-    const textNode = document.createTextNode(`${name} - ${disp} - ${category}`);
-    
+    const textNode = document.createTextNode(`${name}-${disp}-${category}`);
+   // console.log(textNode);
     // Create an <li> element and append the text node
     const li = document.createElement('li');
     li.setAttribute('data-myObj_ser-id', itemId.toString());
@@ -47,7 +49,7 @@ function onSubmit(e){
 
     // Append the <li> element to the list
     item.appendChild(li);
-    console.log(response.data);
+    
     // Clear input values
     nam.value = '';
     dis.value = '';
@@ -82,25 +84,68 @@ function onEdit(e){
         console.log(obj);
     }
 }
-function initializeList() {
-    let listHTML = '';
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const obj = JSON.parse(localStorage.getItem(key));
+// function initializeList() {
+//  axios.get("https://crudcrud.com/api/e955e70c96b34dd99e0cb5bfb20135d2/appoinmentData")
+//     .then((response)=>{
+//         console.log(response);
+//     })
+//     .catch((err)=>{
+// console.log(err);
+//     })
+    // let listHTML = '';
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     const key = localStorage.key(i);
+    //     const obj = JSON.parse(localStorage.getItem(key));
 
-        listHTML += `
-            <li data-myObj_ser-id="${key}">
-                ${obj.name} - ${obj.disp} - ${obj.category}
-                <button class="dltbtn">Delete</button>
-                <button class="edtbtn">Edit</button>
-            </li>
-        `;
-    }
-    item.innerHTML = listHTML;
-}
+    //     listHTML += `
+    //         <li data-myObj_ser-id="${key}">
+    //             ${obj.name} - ${obj.disp} - ${obj.category}
+    //             <button class="dltbtn">Delete</button>
+    //             <button class="edtbtn">Edit</button>
+    //         </li>
+    //     `;
+    // }
+    // item.innerHTML = listHTML;
+//}
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get("https://crudcrud.com/api/e955e70c96b34dd99e0cb5bfb20135d2/appoinmentData")
+    .then((response)=>{
+        console.log(response);
+        for(var i=0;i<response.data.length;i++){
+            const item = response.data[i];
+            const { name, disp, category } = item;
 
+            // Create a list item (<li>) to display the data
+            const li = document.createElement('li');
+            li.textContent = `${name} - ${disp} - ${category}`;
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => {
+               
+               
+            });
 
-initializeList();
+            // Create an edit button
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            editButton.addEventListener('click', () => {
+               
+            });
+            li.appendChild(deleteButton);
+            li.appendChild(editButton);
+
+            // Append the list item to the container
+            itemList.appendChild(li);   
+        }
+    })
+    .catch((err)=>{
+console.log(err);
+    })
+})
+
+// Create a delete button
+
+//initializeList();
 
 
 
